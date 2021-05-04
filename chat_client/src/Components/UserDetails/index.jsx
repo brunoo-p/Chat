49e7b6c18f692c15b3d88e6abea4c57fb75bb586
bruno <brun.op@hotmail.com>
2021-05-03 21:32:53 -0300

    SettingsMenu

--- chat_client/src/Components/UserDetails/index.jsx
+++ chat_client/src/Components/UserDetails/index.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, DotsSettings, SettingMenu, Content, UserImage, UserName } from './styles';
 import avatarDefault from '../../Assets/avatar.png'
 
 export default function UserDetails() {
     
+    const [ showMenu, setShowMenu ] = useState(false);
+
+    const handleMenu = () => {
+        setShowMenu(!showMenu);
+    }
+
     return (
         <Container>
             <Content>
                 <UserImage src={avatarDefault}/>
                 <UserName> Nome usuário</UserName>
 
-                <DotsSettings/>
-                <SettingMenu>
+                <DotsSettings onClick={handleMenu}/>
+                <SettingMenu style={{display: showMenu ? 'flex' : 'none'}}>
                     <ul>
                         <li>Adicionar Contato</li>
                         <li>Limpar Conversa</li>
